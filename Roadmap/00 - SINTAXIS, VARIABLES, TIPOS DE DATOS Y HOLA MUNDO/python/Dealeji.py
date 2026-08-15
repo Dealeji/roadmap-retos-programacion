9a7f711035ba37a375c4f3d627d15ea071e85dad
Dealeji <dealeji1@gmail.com>
2026-08-15 16:29:35 -0500

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Dealeji.py
@@ -0,0 +1,28 @@
+# https://python.org/
+
+# esto es un comentario de una sola línea
+'''esto
+es un comentario
+de varias líneas'''
+
+"""esto
+es un comentario
+de varias líneas"""
+
+my_var = "esta es mi variable"
+my_var_constant = 'esta es mi variable constante'
+
+# las siguientes variables son de tipo primitivo
+my_var_int = 10
+my_var_float = 10.5
+my_var_bool = True
+my_var_bool = False
+my_var_none = None
+my_var_complex = 1 + 2j
+my_var_bytes = b"esta es mi variable bytes"
+my_var_str = "esta es mi variable string"
+
+# el siguiente ejemplo es una impresion en pantalla
+
+print("Hola python!")
+
